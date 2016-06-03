05a59bb9092a7c205273f4a82be2ce8f158da4d5
Aimee Barciauskas <abarciauskas@mdsol.com>
2016-06-03 11:10:53 +0200

    Remove old function

--- src/com/gaceta/Main.java
+++ src/com/gaceta/Main.java
@@ -27,58 +27,12 @@ public class Main {
         return c;
     }
 
-//    public static void processDocuments(boolean writeTimes) {
-//        try {
-//            String filetype = "SPLIT_NORM";
-//            final java.io.File folder = new java.io.File("/Users/aimeebarciauskas/GACETA/" + filetype);
-//            java.io.PrintWriter writer_processing = new java.io.PrintWriter(filetype.toLowerCase() + "_create_corpus_times.csv", "UTF-8");
-//            java.io.PrintWriter writer_alignments = new java.io.PrintWriter(filetype.toLowerCase() + "_alignment_times.csv", "UTF-8");
-//
-//            int limit = 0;
-//
-//            for (final File fileEntry : folder.listFiles()) {
-//                limit++;
-//                if (limit > 10) {
-//                    break;
-//                } else {
-//                    System.out.println("Reading file: " + fileEntry.getName());
-//                    final long startTime = System.currentTimeMillis();
-//                    Corpus corpus;
-//                    corpus = new Corpus(fileEntry.getAbsolutePath());
-//                    final long endTime = System.currentTimeMillis();
-//                    writer_processing.println(fileEntry.getName() + ", " + fileEntry.length() + ", " + (endTime - startTime));
-//
-//                    Alignment alignment = new Alignment();
-//
-//                    for (int i = 0; i < corpus.documents.size(); i++) {
-//                        for (int j = 0; j < corpus.documents.size(); j++) {
-//                            if (i < j) {
-//                                ArrayList doc1 = corpus.documents.get(i);
-//                                ArrayList doc2 = corpus.documents.get(j);
-//                                if (doc1.size() > 0 && doc2.size() > 0) {
-//
-//                                    final long startTimeAl = System.currentTimeMillis();
-//                                    HashMap nw = alignment.needlemanWunsch(doc1, doc2, false);
-//                                    final long endTimeAl = System.currentTimeMillis();
-//                                    writer_alignments.println(doc1.size() + ", " + doc2.size() + ", " + (endTimeAl - startTimeAl));
-//
-//                                    int nwscore = (int) nw.get("score");
-//                                    int[][] nwmatrix = (int[][]) nw.get("matrix");
-//                                    //Alignment.printAlignment(nwmatrix, doc1, doc2);
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            writer_processing.close();
-//            writer_alignments.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            System.err.println("Error: " + e.getMessage());
-//        }
-//    }
+    // finds all documents by filename
+    // calculates alignments and stores them in db
+    public static void calcAndWriteAlignments() {
+        
+
+    }
 
     public static void writeDocuments() {
         try {
